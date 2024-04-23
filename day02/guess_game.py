f31fe24e597a9f3a31761059713ea2ec3e23895a
AdamLiberman <adamlc121@gmail.com>
2024-04-23 17:16:03 +0300

    update

--- day02/guess_game.py
+++ day02/guess_game.py
@@ -4,7 +4,7 @@ random_number = random.randrange(1, 21)
 Number_of_guesses = 1
 
 while True:
-    guess = int(input("guess a number between 1 and 20: "))
+    guess = int(input("Guess a number between 1 and 20: "))
     if guess == random_number:
         print("You win!")
         print(f"Number of guesses is: {Number_of_guesses}")
