699c4437aa9d8b289f87e204eba61e1f455693f8
AdamLiberman <166949535+AdamLiberman@users.noreply.github.com>
2024-04-25 12:02:16 +0300

    Update guess_game.py

--- day02/guess_game.py
+++ day02/guess_game.py
@@ -1,19 +1,19 @@
 import random
 
 random_number = random.randrange(1, 21)
-Number_of_guesses = 1
+number_of_guesses = 1
 
 while True:
     guess = int(input("Guess a number between 1 and 20: "))
     if guess == random_number:
         print("You win!")
-        print(f"Number of guesses is: {Number_of_guesses}")
+        print(f"Number of guesses is: {number_of_guesses}")
         break
     elif guess > random_number:
         print("Your guess is too big")
-        Number_of_guesses +=1
+        number_of_guesses +=1
     elif guess < random_number:
         print("Your guess is too small")
-        Number_of_guesses +=1
+        number_of_guesses +=1
 
 
