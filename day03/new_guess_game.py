fa2e80ca0114aa7db3b4565ca8444bd3216c187d
AdamLiberman <adamlc121@gmail.com>
2024-05-04 18:31:15 +0300

    update game

--- day03/new_guess_game.py
+++ day03/new_guess_game.py
@@ -4,36 +4,43 @@ def com_number():
     random_number = random.randrange(1, 21)
     return random_number
 
-def guessing(random_number, number_of_guesses):
-    while True:
+def guessing(guess, random_number, number_of_guesses):
+    if int(guess) == random_number:
+        print("You win!")
+        print(f"Number of guesses is: {number_of_guesses}")
+        return True
+    elif int(guess) > random_number:
+        print("Your guess is too big") 
+    elif int(guess) < random_number:
+        print("Your guess is too small")
+    
+def game(random_number, number_of_guesses):
+    is_game_over = False
+    while not is_game_over:
         guess = input("Guess a number between 1 and 20: ")
-        number_of_guesses +=1
         if guess=="x":
             break
-        if guess=="n":
+        elif guess=="n":
             restart_game = input("Do you want to play a new game? ")
             if restart_game.lower() == "yes":
                 number_of_guesses = 0
                 random_number = com_number()
                 continue
-            if restart_game.lower() == "no" or "x":
+            elif restart_game.lower() == "no":
+                continue
+            else:    
                 break
-        if guess=="s":
+        elif guess == "s":
             print(f"the number is {random_number}")
             break
-        elif int(guess) == random_number:
-            print("You win!")
-            print(f"Number of guesses is: {number_of_guesses}")
-            break
-        elif int(guess) > random_number:
-            print("Your guess is too big") 
-        elif int(guess) < random_number:
-            print("Your guess is too small")
+        else:
+            number_of_guesses +=1
+            is_game_over = guessing(guess, random_number, number_of_guesses)
 
 def main():
     random_number = com_number()
     number_of_guesses = 0
-    display = guessing(random_number, number_of_guesses)
+    display = game(random_number, number_of_guesses)
     return display
 
 main()
